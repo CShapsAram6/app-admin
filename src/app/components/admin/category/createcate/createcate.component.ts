5de68ce211e323b642f6cc254d6773192db713d9
NguyenTTung <cacppkk2211@gmail.com>
2024-07-16 11:33:47 +0700

    testcreatecate

---
+++ src/app/components/admin/category/createcate/createcate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createcate',
+  templateUrl: './createcate.component.html',
+  styleUrl: './createcate.component.scss'
+})
+export class CreatecateComponent {
+
+}

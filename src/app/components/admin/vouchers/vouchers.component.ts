05f528a4ec7ed712783347e98be0e69b76d57385
Huy Ho <azirkingsand0.12@gmail.com>
2024-07-24 08:28:32 +0700

    view voucher

--- src/app/components/admin/vouchers/vouchers.component.ts
+++ src/app/components/admin/vouchers/vouchers.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { voucherDto } from '../../../model/voucher.model';
+import { VoucherService } from '../../../services/voucher.service';
+import { ApiResponse } from '../../../model/ApiResponse.model';
 
 @Component({
   selector: 'app-vouchers',
   templateUrl: './vouchers.component.html',
   styleUrl: './vouchers.component.scss'
 })
-export class VouchersComponent {
+export class VouchersComponent implements OnInit {
+  constructor (private voucher:VoucherService ){}
+  ngOnInit(): void { 
 
+  }
+  ListVoucher: voucherDto[]=[];
+  GetListVoucher(){
+    return this.voucher.getData().subscribe((data:ApiResponse<voucherDto[]>)=>{
+      console.log(data);
+     this.ListVoucher = data.data;
+    })
+  }
 }
---
+++ src/app/model/voucher.model.ts
@@ -0,0 +1,9 @@
+interface voucherDto {
+    id: number;
+    name: string;
+    status: number;
+    timeStart : string;
+    timeEnd : string;
+    minOrderVaule : number;
+}
+export {voucherDto};

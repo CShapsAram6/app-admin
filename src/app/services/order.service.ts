df6d837e79152833e8b9b6633289b629977ffd1c
TUAN <hoangquoctuans3@gmail.com>
2024-07-25 00:41:16 +0700

    bị lỗi - cancelOder

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -22,12 +22,12 @@ export class OrderService {
     return this.http.put(`${environment.api}/Order/confirm-order-${id}`, {})
   }
 
-  cancelOrder(id: number, reason: string) {
-    const reasonDto: reasonDto = {
-      reasonCancel: reason
-    }
-    return this.http.put(`${environment.api}/Order/cancel-order-${id}`, reasonDto)
-  }
+  // cancelOrder(id: number, reason: string) {
+  //   const reasonDto: reasonDto = {
+  //     reasonCancel: reason
+  //   }
+  //   return this.http.put(`${environment.api}/Order/cancel-order-${id}`, reasonDto)
+  // }
 
   confirmDelivery(id: number) {
     return this.http.put(`${environment.api}/Order/confirm-delivery-${id}`, {})

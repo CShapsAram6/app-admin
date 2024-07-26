8107d08a054456517cfd10955cb3fd55bdc4f434
lesykien <lesykien030@gmail.com>
2024-07-26 22:03:44 +0700

    cc

--- src/app/model/blog.model.ts
+++ src/app/model/blog.model.ts
@@ -1,16 +1,16 @@
-interface blogDto{
-    id: number;
-    header: string;
-    content: string;
-    datePush: string;
-    images:string;
+interface blogDto {
+  id: number;
+  header: string;
+  content: string;
+  datePush: string;
+  images: string;
 }
-interface createblog{
-    header : String;
-    content : String;
-    // chuaw theem accontID
-    images: File[];
+interface createblog {
+  header: String;
+  content: String;
+  // chuaw theem accontID
+  images: File[];
 }
+// up git
 
-
-export { blogDto , createblog};
+export { blogDto, createblog };

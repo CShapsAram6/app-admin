fd381b7c78d445feb4e8c10a25d0968026ac49e4
lesykien <lesykien030@gmail.com>
2024-07-26 22:08:02 +0700

    cc dmmmmmmmmmmm

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -6,19 +6,22 @@ import { ApiResponse } from '../model/ApiResponse.model';
 import { blogDto, createblog } from '../model/blog.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BlogService {
-
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
   getData(page: number): Observable<ApiResponse<blogDto[]>> {
     return this.http.get<ApiResponse<blogDto[]>>(
       `${environment.api}/Blog/getall-blog-${page}`
     );
   }
 
-  createblog(form:FormData): Observable<ApiResponse<createblog>>{
-    return this.http.post<ApiResponse<createblog>>
-    (`${environment.api}/Blog/create-blog`,form);
+  createblog(form: FormData): Observable<ApiResponse<createblog>> {
+    return this.http.post<ApiResponse<createblog>>(
+      `${environment.api}/Blog/create-blog`,
+      form
+    );
   }
 }
+
+// cc

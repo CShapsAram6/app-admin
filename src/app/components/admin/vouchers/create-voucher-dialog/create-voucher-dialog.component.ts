933ebd59957660272c435069a6c5526378a810d4
lesykien <lesykien030@gmail.com>
2024-07-30 10:21:42 +0700

    _fix

--- src/app/components/admin/vouchers/create-voucher-dialog/create-voucher-dialog.component.ts
+++ src/app/components/admin/vouchers/create-voucher-dialog/create-voucher-dialog.component.ts
@@ -1,44 +1,41 @@
 // create-voucher-dialog.component.ts
 
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-voucher-dialog',
   templateUrl: './create-voucher-dialog.component.html',
-  styleUrls: ['./create-voucher-dialog.component.scss']
+  styleUrls: ['./create-voucher-dialog.component.scss'],
 })
 export class CreateVoucherDialogComponent implements OnInit {
   inputdata: any;
   myForm: FormGroup;
 
-  constructor(
-    public dialogRef: MatDialogRef<CreateVoucherDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    private fb: FormBuilder
-  ) {
-    this.myForm = this.fb.group({
-      name: ['', Validators.required],
-      timeStart: ['', Validators.required],
-      timeEnd: ['', Validators.required],
-      discountType: ['', Validators.required],
-      discount: ['', [Validators.required, Validators.min(0)]],
-      min_Order_Value: ['', [Validators.required, Validators.min(0)]],
-      maxDiscount: ['', [Validators.required, Validators.min(0)]],
-      stock: ['', [Validators.required, Validators.min(0)]],
-      status: ['', Validators.required]
-    }, { validators: this.timeRangeValidator });
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        timeStart: ['', Validators.required],
+        timeEnd: ['', Validators.required],
+        discountType: ['', Validators.required],
+        discount: ['', [Validators.required, Validators.min(0)]],
+        min_Order_Value: ['', [Validators.required, Validators.min(0)]],
+        maxDiscount: ['', [Validators.required, Validators.min(0)]],
+        stock: ['', [Validators.required, Validators.min(0)]],
+        status: ['', Validators.required],
+      },
+      { validators: this.timeRangeValidator }
+    );
   }
 
-  ngOnInit(): void {
-    this.inputdata = this.data;
-  }
-
-  close(): void {
-    this.dialogRef.close();
-  }
-
+  ngOnInit(): void {}
   saveVoucher(): void {
     if (this.myForm.valid) {
       console.log(this.myForm.value);
@@ -47,9 +44,13 @@ export class CreateVoucherDialogComponent implements OnInit {
     }
   }
 
-  private timeRangeValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  private timeRangeValidator(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
     const timeStart = control.get('timeStart')?.value;
     const timeEnd = control.get('timeEnd')?.value;
-    return timeStart && timeEnd && timeStart >= timeEnd ? { timeRangeInvalid: true } : null;
+    return timeStart && timeEnd && timeStart >= timeEnd
+      ? { timeRangeInvalid: true }
+      : null;
   }
 }
--- src/app/components/admin/vouchers/vouchers.component.ts
+++ src/app/components/admin/vouchers/vouchers.component.ts
@@ -2,43 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { voucherDto } from '../../../model/voucher.model';
 import { VoucherService } from '../../../services/voucher.service';
 import { ApiResponse } from '../../../model/ApiResponse.model';
-import { CreateVoucherDialogComponent } from './create-voucher-dialog/create-voucher-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import { title } from 'process';
-
 
 @Component({
   selector: 'app-vouchers',
   templateUrl: './vouchers.component.html',
-  styleUrl: './vouchers.component.scss'
+  styleUrl: './vouchers.component.scss',
 })
 export class VouchersComponent implements OnInit {
-  constructor (private voucher:VoucherService, private dialog:MatDialog){}
+  constructor(private voucher: VoucherService) {}
 
-  openDialog(): void {
-   const dialogRef = this.dialog.open(CreateVoucherDialogComponent,{
-      height: '400px',
-      width: '60%',
-      data:{
-        title: 'Thêm Voucher'
-      }
-    });
-    dialogRef.afterClosed().subscribe(result=>{
-      console.log('close');
-      console.log(result);
-    })
-    
-  }
-
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.GetListVoucher();
   }
-  ListVoucher: voucherDto[]=[];
-  GetListVoucher(){
-    return this.voucher.getData().subscribe((data:ApiResponse<voucherDto[]>)=>{
-      console.log(data);
-     this.ListVoucher = data.data;
-    })
+  ListVoucher: voucherDto[] = [];
+  GetListVoucher() {
+    return this.voucher
+      .getData()
+      .subscribe((data: ApiResponse<voucherDto[]>) => {
+        console.log(data);
+        this.ListVoucher = data.data;
+      });
   }
   getStatus(status: number): string {
     switch (status) {
